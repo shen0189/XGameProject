330489c993e83a86fc26180f269c5e9c5d162dca
szl <thuszl784@163.com>
2023-10-25 22:54:47 +0800

    删除main中的多余内容

--- connection/main.py
+++ connection/main.py
@@ -7,29 +7,6 @@ from connection.dataclasses import TrafficFlow, SPAT, Trigger
 from algorithm.mapInfo import intersection
 
 
-# MEC_ID = '1'
-#
-#
-# def create_topic(mec_id: str, _topic: List[str], direction: bool) -> Dict[str, Tuple[str, int]]:
-#     """
-#     按照通信接口协议构造topic列表，从而简化获取和调用topic的code
-#     Args:
-#         mec_id: MEC的id
-#         _topic: 所需订阅或发布的topic项, 如BSM, TrafficFlow, so on
-#         direction: 消息传输方向, True:云平台->MEC, False:MEC->云平台
-#
-#     Returns: topic和qos等级组成的元组列表, 如('MECUpload/<MEC_id>/TrafficFlow', 0)
-#
-#     """
-#     if direction is True:
-#         head = 'MECCloud'
-#         complete_topic = {item: ('/'.join([head, item, mec_id]), 0) for item in _topic}
-#     else:
-#         head = 'MECUpload'
-#         complete_topic = {item: ('/'.join([head, mec_id, item]), 0) for item in _topic}
-#     return complete_topic
-
-
 def on_connect(client, user_data, flags, rc):
     if rc == 0:
         print('Connect to MQTT Broker')
@@ -97,10 +74,8 @@ def connect(topics: Union[str, Iterable[str]], trigger: Trigger):
     client.loop_forever()
 
 
-
-
 if __name__ == '__main__':
-
+    
     trigger = Trigger()
 
     topics = ['MECCloud/1/TrafficFlow', 'MECCloud/1/SPAT']
